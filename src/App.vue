524aaa5994cc22979b0a6739a233d4a427df1da0
Anusha Kamat <anushasanyamkamat@gmail.com>
2021-10-31 19:39:29 +0100

    add timer status message block

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <button v-on:click="startTimer">Start timer</button>
     <button v-on:click="pauseTimer">Pause timer</button>
     <button v-on:click="clearTimer">Clear timer</button>
+    <div>{{message}}</div>
   </div>
 </template>
 
@@ -14,6 +15,7 @@ export default {
     return {
       timerValue: 0,
       timerId: null,
+      message: 'Click start to start the timer',
     }
   },
   components: {},
@@ -22,13 +24,16 @@ export default {
       this.timerId = setInterval(()=>{
         this.timerValue++;
       }, 1000);
+      this.message = "The timer has started"
     },
     pauseTimer() {
       clearInterval(this.timerId);
+      this.message = "The timer has been paused"
     },
     clearTimer() {
       clearInterval(this.timerId);
       this.timerValue = 0;
+      this.message = "Click start to start the timer"
     },
   },
 }
