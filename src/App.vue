d52d4178c237f7170c90c2ed8c35e19f14c22a84
Anusha Kamat <anushasanyamkamat@gmail.com>
2021-10-31 19:18:23 +0100

    add vue init files

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div>{{timerValue}}</div>
+    <button v-on:click="startTimer">Start timer</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      timerValue: 0,
+      timerId: null,
+    }
+  },
+  components: {},
+  methods:{
+    startTimer() {
+      this.timerId = setInterval(()=>{
+        this.timerValue++;
+      }, 1000);
+    },
+  },
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
