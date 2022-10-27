825b2975b5fa318cf9fa5951f5a5aea8b8675690
Anusha Kamat <anushasanyamkamat@gmail.com>
2022-10-27 17:08:41 +0530

    Update index.ts (#8)
    Update the store which will helps us update the user information

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    userInfo: {
+      name: '',
+      email: '',
+    },
   },
   mutations: {
+    updateUser: (state, userDetails) => {
+      state.userInfo.name = userDetails.name;
+      state.userInfo.email = userDetails.email;
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {}
+});
