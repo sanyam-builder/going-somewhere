83913e888434c2e99f7973b6e347f3f2e7479772
Anusha Kamat <anushasanyamkamat@gmail.com>
2022-10-27 17:16:00 +0530

    Extract timer and use it in the main App (#9)
    * Create Timer.vue
    
    extract timer component
    
    * Update App.vue
    
    Update App to use the new timer component

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,20 @@
 <template>
   <div id="app">
-    <div>{{timerValue}}</div>
-    <button v-on:click="startTimer">Start timer</button>
-    <button v-on:click="pauseTimer">Pause timer</button>
-    <button v-on:click="clearTimer">Clear timer</button>
-    <div>{{message}}</div>
+    <timer/>
   </div>
 </template>
 
 <script>
+import Timer from '@/components/Timer';
+
 export default {
   name: 'App',
   data: () => {
-    return {
-      timerValue: 0,
-      timerId: null,
-      message: 'Click start to start the timer',
-    }
+    return {};
   },
-  components: {},
-  methods:{
-    startTimer() {
-      this.timerId = setInterval(()=>{
-        this.timerValue++;
-      }, 1000);
-      this.message = "The timer has started"
-    },
-    pauseTimer() {
-      clearInterval(this.timerId);
-      this.message = "The timer has been paused"
-    },
-    clearTimer() {
-      clearInterval(this.timerId);
-      this.timerValue = 0;
-      this.message = "Click start to start the timer"
-    },
-  },
-}
+  components: { Timer },
+  methods: {},
+};
 </script>
 <style lang="scss">
 #app {
---
+++ src/components/Timer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>{{ timerValue }}</div>
+    <button v-on:click="startTimer">Start timer</button>
+    <button v-on:click="pauseTimer">Pause timer</button>
+    <button v-on:click="clearTimer">Clear timer</button>
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timer',
+  data: () => {
+    return {
+      timerValue: 0,
+      timerId: null,
+      message: 'Click start to start the timer',
+    };
+  },
+  methods: {
+    startTimer() {
+      this.timerId = setInterval(() => {
+        this.timerValue++;
+      }, 1000);
+      this.message = 'The timer has started';
+    },
+    pauseTimer() {
+      clearInterval(this.timerId);
+      this.message = 'The timer has been paused';
+    },
+    clearTimer() {
+      clearInterval(this.timerId);
+      this.timerValue = 0;
+      this.message = 'Click start to start the timer';
+    },
+  },
+};
+</script>
