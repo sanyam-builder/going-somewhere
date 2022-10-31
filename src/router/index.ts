f34566885b7b66ad7b9f557f8b8d8817d9008123
sanyam-builder <117102952+sanyam-builder@users.noreply.github.com>
2022-10-31 21:40:41 +0530

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
 import GetStartedPage from '../views/GetStartedPage.vue'
+import Settings from '../views/Settings.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes: Array<RouteConfig> = [
     path: '/get-started',
     name: 'GetStarted',
     component: GetStartedPage,
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings,
   },
   {
     path: '/about',
